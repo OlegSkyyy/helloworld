6de4b04bcb2ee016bfcc28f3d8ec96d66304a5fb
OlegSkyyy <e.chshegoleva@sibstrin.ru>
2023-04-26 23:27:00 +0700

    first project

---
+++ hello/hello/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Uh, hi, Mark!");
+            Console.ReadKey();
+        }
+    }
+}
